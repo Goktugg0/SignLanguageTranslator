74a9ab4fa16d1dee688d2c098a1d3fbe9e91c35a
Goktugg0 <goktugcakmakli35@gmail.com>
2025-08-12 00:06:13 +0300

    Perfect access to the camera

--- translator.py
+++ translator.py
@@ -1,1 +1,13 @@
 import cv2 as cv
+import numpy as np
+
+cap = cv.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    cv.imshow('frame', frame)
+
+    if cv.waitKey(1) == ord('q'):
+        break
+cap.release()
+cv.destroyAllWindows()
