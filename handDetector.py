52e70b9029fb132c7becc3f2160c74b634168be9
Goktugg0 <goktugcakmakli35@gmail.com>
2025-08-12 22:57:00 +0300

    The file is moved and succesfully getting the hand coordinates when there is

--- handDetector.py
+++ handDetector.py
@@ -1,1 +1,31 @@
 import mediapipe as mp
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(static_image_mode=False,
+                      max_num_hands=2,
+                      model_complexity=1,
+                      min_detection_confidence=0.5,
+                      min_tracking_confidence=0.5
+                      )
+
+
+while True:
+    ret, frame = cap.read()
+
+    mirrored_frame = cv.flip(frame, 1)
+
+    # Convert BGR (OpenCV format) to RGB (MediaPipe format)
+    frameRGB = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    # Process with the hands
+    results = hands.process(frameRGB)
+
+    print(results.multi_hand_landmarks)
+
+    cv.imshow('frame', mirrored_frame)
+
+    if cv.waitKey(1) == ord('q'):
+        break
+cap.release()
+cv.destroyAllWindows()
--- translator.py
+++ translator.py
@@ -1,16 +1,3 @@
 import cv2 as cv
 import numpy as np
 
-cap = cv.VideoCapture(0)
-
-while True:
-    ret, frame = cap.read()
-
-    mirrored_frame = cv.flip(frame,1)
-    
-    cv.imshow('frame', mirrored_frame)
-
-    if cv.waitKey(1) == ord('q'):
-        break
-cap.release()
-cv.destroyAllWindows()
