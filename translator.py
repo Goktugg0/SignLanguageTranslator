b455dc1a19958337207686e9e75021f2ea4ae624
Goktugg0 <goktugcakmakli35@gmail.com>
2025-08-23 01:38:51 +0300

    last pydoc will do readme

--- translator.py
+++ translator.py
@@ -6,41 +6,55 @@ import numpy as np
 
 from handDetector import HandDetector
 
-model = keras.models.load_model("model.keras")
-with open("scaler.pkl", "rb") as f:
-    scaler = pickle.load(f)
-with open("label_encoder.pkl", "rb") as f:
-    le = pickle.load(f)
+def load():
+    # Load the model, scaler, and encoder
+    model = keras.models.load_model("model.keras")
+    with open("scaler.pkl", "rb") as f:
+        scaler = pickle.load(f)
+    with open("label_encoder.pkl", "rb") as f:
+        le = pickle.load(f)
+    return model, scaler, le
 
-detector = HandDetector()
+def predict(model, le, landmarks_scaled):
+    """This function takes the scaled landmarks and use for prediction"""
+    predicted = model.predict(landmarks_scaled, verbose=0)
+    predicted_index = np.argmax(predicted)
+    predicted_label = le.inverse_transform([predicted_index])[0]  # decode label
+    # Gives the probability of the maximum likelihood element
+    confidence = np.max(predicted)
+    return predicted_label, confidence
 
-while True:
-    frame, results = detector.read_frame()
-    if frame is None:
-        break
+def main():
+    model, scaler, le = load()
+    detector = HandDetector()
 
-    landmark_vector = detector.landmarks_to_list(results)
+    while True:
+        frame, results = detector.read_frame()
+        if frame is None:
+            break
+
+        landmark_vector = detector.landmarks_to_list(results)
 
 
-    if landmark_vector is not None and len(landmark_vector) >= 63:
-        # Convert to array and reshape
-        landmarks = np.array(landmark_vector[:63], dtype=np.float32).reshape(1, 63)
+        if landmark_vector is not None and len(landmark_vector) >= 63:
+            # Convert to array and reshape
+            landmarks = np.array(landmark_vector[:63], dtype=np.float32).reshape(1, 63)
 
-        # Scale features using the saved scaler
-        landmarks_scaled = scaler.transform(landmarks)
+            # Scale features using the saved scaler
+            landmarks_scaled = scaler.transform(landmarks)
 
-        # Predict
-        predicted = model.predict(landmarks_scaled, verbose=0)
-        predicted_index = np.argmax(predicted)
-        predicted_label = le.inverse_transform([predicted_index])[0]  # decode label
-        confidence = np.max(predicted)
+            predicted_label, confidence = predict(model, le, landmarks_scaled)
 
-        text = f"{predicted_label} ({confidence:.2f})"
-        cv.putText(frame, text, (10, 40), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-    cv.imshow("Sign Language Translator", frame)
+            text = f"{predicted_label} ({confidence:.2f})"
+            # Put it on the screen
+            cv.putText(frame, text, (10, 40), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        cv.imshow("Sign Language Translator", frame)
 
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
 
-detector.release()
+    detector.release()
+
+if __name__ == "__main__":
+    main()
 
